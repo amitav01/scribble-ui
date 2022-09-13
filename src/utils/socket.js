ab3495381d6307e0cce704e297436acf3b75319a
Amitav Mishra <amitavmishra99@gmail.com>
2022-09-13 18:26:11 +0530

    revert socket address

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -2,7 +2,7 @@ import { io } from 'socket.io-client';
 
 let socket;
 const SOCKET_SERVER =
-  process.env.NODE_ENV === 'development'
+  process.env.NODE_ENV === 'production'
     ? 'https://scribble-server.herokuapp.com/'
     : 'http://localhost:8080';
 

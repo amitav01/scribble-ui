d024504c3693840db4e6bb4ee0085e4882e56f74
Amitav Mishra <amitavmishra99@gmail.com>
2023-02-12 00:14:31 +0530

    Updated servide Endpoint!!

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -3,7 +3,7 @@ import { io } from 'socket.io-client';
 let socket;
 const SOCKET_SERVER =
   process.env.NODE_ENV === 'production'
-    ? 'https://scribble-server.herokuapp.com/'
+    ? 'https://scribble-service.onrender.com'
     : 'http://localhost:8080';
 
 export const initSocket = () => {
